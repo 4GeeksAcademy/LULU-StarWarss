38087851be986f1cb4dec0ff9c6727d5912f6fe4
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-04-01 00:19:22 +0000

    it needs most work

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+///FOOTER WILL HAVE: 
+//1. StartWars logo//
+//2. Botton with Favorite Dropdown y button inside to delete favorites//
+
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-light bg-light mb-3">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO1zFjG1fuitDiUaDDv_M8WNLUiBqx1c8fgjyHvB4Ngw&s" alt="Logo" style={{ width: '50px', height: 'auto', marginLeft: '50px' }} />
+            <div className="ml-auto">
+                <Link to="/demo">
+                    <button className="btn btn-primary">Check the Context in action</button>
+                </Link>
+            </div>
+        </nav>
+    );
 };
+
